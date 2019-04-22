cd7f96a1336fb025fd6340850b1fe1a4e10e84f5
Rayan702 <49521780+Rayan702@users.noreply.github.com>
2019-04-22 22:50:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,116 +1,14 @@
 const Discord = require('discord.js');
-
-const fs = require('fs')
-
 const client = new Discord.Client();
 
-                    
-
-const prefix = '!';
-
 client.on('ready', () => {
-
-  
-    console.log(`Logged in as [ FOIL]`);
-
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const adminprefix = "!";
-const developers = ['518468852976451614'];
-
-console.log("FOIL");
-
-client.on('ready', () => {
-    console.log(`Logged as ${client.user.tag}`)
-})
-
-
-client.on('message', message => {//// حقوق @! FOIL 
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**:white_check_mark:  تم تغيير البلاينق الى   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "باي")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**:white_check_mark:  تَم تغيير الواتشينق الى   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**:white_check_mark:  تَم تغيير الليسينينق الى   ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
-      message.channel.send(`**:white_check_mark:  تم تغييرك حالتك بالتويتش الى   ${argresult}**`)
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
   }
-  if (message.content.startsWith(adminprefix + 'name')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`**:white_check_mark:  جاري تغيير الأسم لـ ..${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'avatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`**:white_check_mark:  جاري تغيير الأفتار... :** `);
-}
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.login(process.env.BOT_TOKEN);
+client.login('NTY1ODYwOTQ4NDczMDIwNDE2.XL3XOw.hkKDPpjfskmdjTkbhsmRs2W9azw');
